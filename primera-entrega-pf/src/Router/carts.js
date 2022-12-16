47d5366232c01006fec7656f93afcc1daa5fec76
Augustolm <augustomosettig@yahoo.com.ar>
2022-12-15 22:37:12 -0300

    primera parte de la primera entrega

---
+++ primera-entrega-pf/src/Router/carts.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+//import {} from "../controller/carts.js";
+
+
+const routerCarts = Router();
+
+
+
+//routerCarts.get("/:cid", cartsID);
+//routerCarts.post("/:cid/product/:pid", cartsPost);
+//routerCarts.put("/", cartsPut);
+//routerCarts.delete("/:id", cartsDelete);
+
+
+export default routerCarts;
